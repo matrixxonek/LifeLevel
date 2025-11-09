68828b89e5bf6288b2b1787922279d6b90ca07ed
Mateusz Turbański <matrixxon@gmail.com>
2025-11-09 17:30:43 +0100

    import rbcdnd i dodawnie typów

---
+++ frontend/src/assets/components/CalendarComponent.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useCallback, useMemo } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+import type { CalendarItem } from '../types/types';
+
+const localizer = momentLocalizer(moment);
+
+const DnDCalendar = withDragAndDrop<CalendarItem, moment.Moment>(Calendar);
+
+const CalendarComponent = () => {
+    const [myCalendarItems, setMyCalendarItems] = useState<CalendarItem[]>([]);
+    //Trzeba MyCalendarItems setować z danych odebranych z bazy danych
+
+    const onEventDrop = ()=>{
+
+    }
+    const onEventResize = ()=>{
+        
+    }
+
+    return (
+        <div style={{ height: '700px' }}>
+            <DnDCalendar
+                localizer={localizer}
+                events={myCalendarItems}
+                onEventDrop={onEventDrop}
+                onEventResize={onEventResize}
+                defaultView="month"
+                startAccessor={(item: CalendarItem) => item.start}
+                endAccessor={(item: CalendarItem) => item.end}
+                style={{ height: 500 }}
+            />
+        </div>
+    );
+}
+
+export default CalendarComponent;
--- frontend/src/assets/types/types.ts
+++ frontend/src/assets/types/types.ts
@@ -1,9 +1,30 @@
-export interface EventApiInterface{
-    id: number;
-    name: string;
-    value: number;
+interface ItemBase {
+  id: string;
+  title: string;
+  description: string;
+  //cykliczność
 }
-export interface EventInterface{
-    name: string;
-    value: number;
+
+interface CalendarEvent extends ItemBase {
+    type: 'event';
+    start: Date;
+    end: Date;
+    isAllDay?: boolean;
 }
+interface CalendarTask extends ItemBase {
+    type: 'task';
+    deadline: Date;
+    status: 'To Do' | 'In Progress' | 'Done';
+    category: 'Mind' | 'Physical' | 'Social';
+    start: Date;
+    end: Date;
+}
+
+export type CalendarItem = CalendarEvent | CalendarTask;
+
+//Typy Wejściowe dla danych od użytkownika
+
+type CreateEvent = Omit<CalendarEvent, 'id' | 'type'>;
+type CreateTask = Omit<CalendarTask, 'id' | 'start' | 'end' | 'type'>;
+
+export type CreateItem = { type: 'event', data: CreateEvent } | { type: 'task', data: CreateTask };
